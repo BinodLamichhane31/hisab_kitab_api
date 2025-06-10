c149b5eb4f487e4cc0382de5907291efcb73d0c4
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-10 20:59:14 +0545

    Rate limit added for login api

---
+++ middlewares/loginLimiter.js
@@ -0,0 +1,14 @@
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: 'Too many failed login attempts from this IP, please try again after 15 minutes.',
+  handler: (req, res, next) => {
+    res.status(429).json({ success: false, message: 'Too many login attempts. Please try again later.' });
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = loginLimiter;
