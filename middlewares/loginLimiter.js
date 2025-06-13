cdb8b720cf9b4289db5ae9f4abae32e6d8b8c793
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-13 16:32:50 +0545

    minor improvements

--- middlewares/loginLimiter.js
+++ middlewares/loginLimiter.js
@@ -5,7 +5,7 @@ const loginLimiter = rateLimit({
   max: 5,
   message: 'Too many failed login attempts from this IP, please try again after 15 minutes.',
   handler: (req, res, next) => {
-    res.status(429).json({ success: false, message: 'Too many login attempts. Please try again later.' });
+    res.status(429).json({ success: false, message: 'Too many login attempts. Please try again after 15 minutes.' });
   },
   standardHeaders: true,
   legacyHeaders: false,
