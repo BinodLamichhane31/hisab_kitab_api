3ba900d84c2569222d8d9f605c7beb36bbbf58c0
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-13 16:32:50 +0545

    minor improvements

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require("./routes/userRoutes")
 const helmet = require('helmet')
-
-
+const cors = require('cors');
 
 dotenv.config();
+const PORT = process.env.PORT || 6060;
 
 const app = express();
 app.use(helmet())
-const PORT = process.env.PORT || 6060;
 
 connectDB();
 
-const cors = require('cors');
 let corsOptions = {
     origin : '*',
  }
  
- app.use(cors(corsOptions))
-
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.use("/api/auth",userRoutes)
