6d9c7ca07c3480154bc9468faf03c303e06bb2dd
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-22 00:48:21 +0545

    Added upload middleware

--- models/User.js
+++ models/User.js
@@ -27,6 +27,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profileImage: {
+      type:String,
+      default:""
+    },
     shops: [
       {
         type: mongoose.Schema.Types.ObjectId,
