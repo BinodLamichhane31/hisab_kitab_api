d4bb1020ae0bbe365d709cada3b254c9f320c589
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-22 00:18:58 +0545

    Added the functions and routes to update, delete and get one shop by id

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,3 +1,4 @@
+const { log } = require("winston");
 const Shop = require("../models/Shop");
 const User = require("../models/User");
 
@@ -73,3 +74,124 @@ exports.getShops = async (req,res) => {
     }
 
 }
+
+exports.getShopById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const shop = await Shop.findById(id).populate({
+      path: 'owner',
+      select: 'fname lname email'
+    });
+
+    if (!shop) {
+      return res.status(404).json({
+        success: false,
+        message: "Shop not found.",
+      });
+    }
+
+    const shopOwnerId = shop.owner._id ? shop.owner._id.toString() : shop.owner.toString();
+    if (shopOwnerId !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Shop data fetched.",
+      data: shop
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error: " + error.message,
+    });
+  }
+};
+
+exports.updateShop = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+
+    const shop = await Shop.findById(id).populate({
+      path: 'owner',
+      select: 'fname lname email'
+    });
+
+    if (!shop) {
+      return res.status(404).json({
+        success: false,
+        message: "Shop not found.",
+      });
+    }
+
+    const shopOwnerId = shop.owner._id ? shop.owner._id.toString() : shop.owner.toString();
+    if (shopOwnerId !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied.",
+      });
+    }
+
+    Object.assign(shop, updates);
+    await shop.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Shop updated.",
+      data: shop
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error: " + error.message,
+    });
+  }
+};
+
+exports.deleteShop = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const shop = await Shop.findById(id).populate({
+      path: 'owner',
+      select: 'fname lname email'
+    });
+
+    if (!shop) {
+      return res.status(404).json({
+        success: false,
+        message: "Shop not found.",
+      });
+    }
+
+    const shopOwnerId = shop.owner._id ? shop.owner._id.toString() : shop.owner.toString();
+    if (shopOwnerId !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied.",
+      });
+    }
+
+    await shop.deleteOne()
+
+    await User.findByIdAndUpdate(req.user._id, { $pull: { shops: shop._id } });
+
+    return res.status(200).json({
+      success: true,
+      message: "Shop deleted.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error: " + error.message,
+    });
+  }
+};
+
+
